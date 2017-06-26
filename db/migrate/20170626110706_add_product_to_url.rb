826473fddbb52daf1bc1e3255f2f52662286d66d
sachin <sachinsonu1994@gmail.com>
2017-06-26 18:42:58 +0530

    cleanup

--- db/migrate/20170626110706_add_product_to_url.rb
+++ db/migrate/20170626110706_add_product_to_url.rb
@@ -1,6 +1,6 @@
 class AddProductToUrl < ActiveRecord::Migration[5.0]
   def change
-  	 create_table :urls do |t|
+  	create_table :urls do |t|
       t.text :original_url
       t.string :short_url
       t.string :sanitize_url
